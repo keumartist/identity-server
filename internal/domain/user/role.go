eda1991c0c095c4073b18ffed9403606f77568c2
JSKeum <jskeum2512@gmail.com>
2023-06-08 18:45:21 +0900

    :art: refactor user domain

---
+++ internal/domain/user/role.go
@@ -0,0 +1,24 @@
+package domain
+
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type Role struct {
+	gorm.Model
+	Name   string `gorm:"type:varchar(256)"`
+	UserID uint
+}
+
+func (r *Role) AssignToUser(user *User) error {
+	for _, existingRole := range user.Roles {
+		if existingRole.Name == r.Name {
+			return errors.New("The user already has this role")
+		}
+	}
+
+	user.Roles = append(user.Roles, *r)
+	return nil
+}
---
+++ internal/domain/user/social.go
@@ -0,0 +1,31 @@
+package domain
+
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type SocialProvider struct {
+	gorm.Model
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"type:varchar(256)"`
+}
+
+type SocialConnection struct {
+	gorm.Model
+	UserID           uint
+	SocialProviderID uint
+	SocialMediaID    string `gorm:"type:varchar(256)"`
+}
+
+func (sc *SocialConnection) Connect(user *User, provider *SocialProvider) error {
+	for _, existingConnection := range user.SocialConnections {
+		if existingConnection.SocialProviderID == provider.ID {
+			return errors.New("The user is already connected to this social media account")
+		}
+	}
+
+	user.SocialConnections = append(user.SocialConnections, *sc)
+	return nil
+}
